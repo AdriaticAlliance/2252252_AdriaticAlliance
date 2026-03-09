86df8a10f576a94d17767ab390779e67f53191ed
Ermin Lilaj <ermin.lilaj04@gmail.com>
2026-03-09 22:00:56 +0100

    fix: resolve missed config.js merge conflict

--- source/rules-service/src/config.js
+++ source/rules-service/src/config.js
@@ -1,11 +1,5 @@
 require('dotenv').config();
 
-<<<<<<< HEAD
-module.exports = {
-  PORT:                    parseInt(process.env.PORT || '4000'),
-  KAFKA_BROKER:            process.env.KAFKA_BROKER || 'localhost:9092',
-  SIMULATOR_URL:           process.env.SIMULATOR_URL || 'http://localhost:8080',
-=======
 const SIMULATOR_HOST = process.env.SIMULATOR_HOST || 'simulator';
 const SIMULATOR_PORT = process.env.SIMULATOR_PORT || '8080';
 const RULES_SERVICE_HOST = process.env.RULES_SERVICE_HOST || 'rules-service';
@@ -18,7 +12,6 @@ module.exports = {
   KAFKA_BROKER:            process.env.KAFKA_BROKER || `${KAFKA_HOST}:${KAFKA_PORT}`,
   SIMULATOR_URL:           process.env.SIMULATOR_URL || `http://${SIMULATOR_HOST}:${SIMULATOR_PORT}`,
   RULES_SERVICE_BASE_URL:  process.env.RULES_SERVICE_BASE_URL || `http://${RULES_SERVICE_HOST}:${RULES_SERVICE_PORT}`,
->>>>>>> origin/network
   DB_PATH:                 process.env.DB_PATH || './data/rules.db',
   KAFKA_TOPIC_NORMALIZED:  process.env.KAFKA_TOPIC_NORMALIZED || 'mars.common-data-records',
   KAFKA_TOPIC_WARNINGS:    process.env.KAFKA_TOPIC_WARNINGS   || 'mars.common-data-records-warnings',
